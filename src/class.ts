2d0a34b1247bb5427163aa8aa2daa864a376f2a6
Amit Kumar <amitgarg.cse@gmail.com>
2020-12-09 16:43:06 +0530

    Private properties and methods

--- src/class.ts
+++ src/class.ts
@@ -1,5 +1,6 @@
 class Department {
     name: string;
+    private employees: string[] = [];
 
     constructor(name: string) {
         this.name = name;
@@ -8,9 +9,27 @@ class Department {
     describe (this: Department) {
         console.log(`Department: ${this.name}`);
     }
+
+    addEmployee (name: string) {
+        this.employees.push(name);
+    }
+
+    printEmployees = () => {
+        console.log(this.employees.length);
+        console.log(this.employees);
+        this.myPrivateMethod();
+    }
+
+    private myPrivateMethod = () => {
+        console.log('My private method');
+    }
 }
 
 const financeDept = new Department('Finance1');
+financeDept.addEmployee('Amit');
+financeDept.addEmployee('Kumar');
+
 console.log(financeDept);
 financeDept.describe();
+financeDept.printEmployees();
 
